0918f679a53806ad9f155f2dfe26a4f8510f9e50
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2022-10-19 00:12:22 -0400

    fixes

--- polls/tests.py
+++ polls/tests.py
@@ -12,6 +12,6 @@ class QuestionModelTests(TestCase):
         was_published_recently() returns False for questions whose pub_date
         is in the future.
         """
-        time = timezone.now() + datetime.timedelta(days=30)
+        time = timezone.now() + datetime.timedelta(days=300)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
